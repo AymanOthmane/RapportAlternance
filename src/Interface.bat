fafe352974edd702bcd61e229ba298a34f899146
Ayman Othmane <148972573+AymanOthmane@users.noreply.github.com>
2024-09-11 18:52:57 +0200

    update

---
+++ src/Interface.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Launching Streamlit interface...
+
+python -m streamlit run interface.py
+
+pause
---
+++ src/Librairie.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Installing necessary libraries...
+
+pip install streamlit
+pip install pandas
+pip install numpy
+pip install matplotlib
+pip install python-dateutil
+pip install QuantLib
+pip install decimal
+pip install scipy
+pip install networkx
+
+echo All libraries have been installed.
+pause
